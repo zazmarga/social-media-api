dd1e00099e0b548053b68bddaf7f79744d850e47
Marharyta Zazulia <zazmargo@gmail.com>
2025-01-09 14:51:50 -0300

    add media file support & install pillow

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from social_media_api import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/user/", include("user.urls"), name="user"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
